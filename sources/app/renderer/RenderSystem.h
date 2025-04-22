4980fdb3a5e6a56b21e2f59d78e4c0216c9ede11
silent1603 <trung.daoquoc@gameloft.com>
2025-04-22 18:25:45 +0700

    * draft renderer system

--- sources/app/renderer/RenderSystem.h
+++ sources/app/renderer/RenderSystem.h
@@ -6,10 +6,14 @@ enum class RendererBackend
     NONE = 0,
     OPENGL,
     OPENGLES,
+    WEBGL,
+    WEBGL2,
+    WEBGPU,
     VULKAN,
     METAL,
-    DIRECTX11,
+    DIRECTX9,
     DIRECTX10,
+    DIRECTX11,
     DIRECTX12,
     SIZE
 };
@@ -19,7 +23,26 @@ void RenderSystem_Init(RendererBackend backendId)
     RendererInit[(int)backendId]();
 }
 
-void (*RendererInit[(int)RendererBackend::SIZE])() = {NULL};
+void DummyRenderer_Init()
+{
+    
+}
+
+//map compile time to function call
+void (*RendererInit[(int)RendererBackend::SIZE])() = {
+    DummyRenderer_Init , // NONE
+    DummyRenderer_Init , // OPENGL
+    DummyRenderer_Init , //OPENGLES
+    DummyRenderer_Init , //WEBGL 
+    DummyRenderer_Init , //WEBGL2 
+    DummyRenderer_Init , //WEBGPU
+    DummyRenderer_Init , //VULKAN
+    DummyRenderer_Init ,  //METAL
+    DummyRenderer_Init ,  //DIRECTX9
+    DummyRenderer_Init ,  //DIRECTX10
+    DX11Renderer_Init ,  //DIRECTX11
+    DummyRenderer_Init ,  //DIRECTX12
+};
 
 
 #endif
